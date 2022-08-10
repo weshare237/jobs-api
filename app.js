cb31005c8232aa170256db720cbbd21e1a9b6a33
weshare237 <fopaduclair2000@gmail.com>
2022-08-10 20:47:51 +0100

    Extra security

--- app.js
+++ app.js
@@ -8,13 +8,28 @@ const connectDB = require('./db/connect')
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // middlewares
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const authMiddleware = require('./middleware/authentication')
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+)
 app.use(express.json())
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -15,14 +15,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.message = Object.values(err.errors)
       .map((item) => item.message)
       .join(',')
-    customError.statusCode = 400
+    customError.statusCode = StatusCodes.BAD_REQUEST
     console.log(customError.message)
   }
   if (err.code && err.code === 11000) {
     customError.message = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
-    customError.statusCode = 400
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }
+
+  if (err.name === 'CastError') {
+    customError.message = `No item found with id : ${err.value}`
+    customError.statusCode = StatusCodes.NOT_FOUND
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.message })
